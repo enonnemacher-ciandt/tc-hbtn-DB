143c18f89721c92824fd65ada96fefd7e253b864
enonnemacher-ciandt <enonnemacher@ciandt.com>
2022-06-27 11:29:46 -0300

    Task 1-Conectando e criando um banco de dados - SQLite

---
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionSQLite {
+
+    public static void main(String[] args) {
+        initConnection();
+    }
+
+    public static void initConnection() {
+        Connection connection = null;
+        try {
+            String url = "jdbc:sqlite:sqlite_database_2022.db";
+            connection = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
